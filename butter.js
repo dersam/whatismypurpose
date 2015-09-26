6cc743c5913975bda30f2f10a1f1431e7cf7e0c1
Sam Schmidt <samuel@dersam.net>
2015-09-25 20:08:21 -0400

    You pass butter.

--- butter.js
+++ butter.js
@@ -1,11 +1,30 @@
+/**
+ * Made without jQuery because arbitrary restrictions
+ * are fun. I guess.
+ */
 var butter = (function(){
+    /**
+     * SHOW ME WHAT YOU GOT
+     */
     function onPurposeRequest() {
+        var $verb = document.getElementById('verb');
+        var $noun = document.getElementById('noun');
 
+        $noun.innerHTML = getNounFromDictionary();
+        $verb.innerHTML = getVerbFromDictionary();
     }
 
     function init() {
         var $purpose = document.getElementById('purpose');
         $purpose.addEventListener('click', onPurposeRequest);
+    }
+
+    function getNounFromDictionary() {
+        return 'butter';
+    }
+
+    function getVerbFromDictionary() {
+        return 'pass';
     }
 
     /**
